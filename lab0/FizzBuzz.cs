f8ee8e164e68dac2f0fe3546dda9a0c1a03b0612
Krzysztof Dryjas <kdryjaskd@gmail.com>
2025-03-06 16:09:17 +0100

    Dodaj pliki projektów.

---
+++ lab0/FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+    class FizzBuzz
+    {
+        int zakres;
+
+        public FizzBuzz(int zakres)
+        {
+            this.zakres = zakres;
+        }
+
+        public void Wyswietlanie()
+        {
+            for (int i = 1; i <= zakres; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ lab0/Program.cs
@@ -0,0 +1,24 @@
+﻿//namespace lab0
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
+
+namespace lab0
+{
+    class Program
+    {
+        static void Main()
+        {
+            int zakres = int.Parse(Console.ReadLine());
+
+            FizzBuzz fizzbuzz = new FizzBuzz(zakres);
+            fizzbuzz.Wyswietlanie();
+        }
+    }
+}
